8301b89a4196aba9e6d173959dd62dac03a8af31
mustafasakalli <mustafasakalli42@gmail.com>
2023-03-25 19:50:20 +0300

    Add NewNote component to notes route

---
+++ components/NewNote.jsx
@@ -0,0 +1,26 @@
+import styles from './NewNote.css';
+
+function NewNote() {
+  return (
+    <form method="post" id="note-form">
+      <p>
+        <label htmlFor="title">Title</label>
+        <input type="text" id="title" name="title" required />
+      </p>
+      <p>
+        <label htmlFor="content">Content</label>
+        <textarea id="content" name="content" rows="5" required />
+      </p>
+      <div className="form-actions">
+        <button>Add Note</button>
+      </div>
+    </form>
+  );
+}
+
+export default NewNote;
+
+//if file does not a route file, remix won't recognize below link
+export function links() {
+  return [{ rel: 'stylesheet', href: styles }];
+}
